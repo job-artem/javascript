6f9a9bd9999fd0e7d047440947dbaae4a272e0e8
job-artem <job.artem.2019@gmail.com>
2020-03-29 12:52:27 +0300

    1

--- lesson2/index.js
+++ lesson2/index.js
@@ -1,7 +1,7 @@
 const userName = 'John';
 const greeting = "I'm John";
 const profession = 'Developer';
-const $hobby = 'Walking';
+const hobby = 'Walking';
 const favouriteDessert = 'cake';
 let pet_ = 'dog';
 let superNumber;
