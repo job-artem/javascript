721920ebba8512e3491325fea768e1cd5dc8c7e7
job-artem <job.artem.2019@gmail.com>
2020-03-30 12:07:04 +0300

    task2 lesson3

---
+++ lesson3/task2/index.js
@@ -0,0 +1,30 @@
+//code should be finished ;)
+
+console.log("17 to string is " + String(17));
+console.log("-17.17 to string is " + String(-17.17));
+console.log("false to string is " + String(false));
+console.log("null to string is " + String(null));
+console.log("undefined to string is " + String(undefined));
+console.log("0 to string is " + String(0));
+
+/* === */
+
+console.log("'17' to number is ", Number('17'));
+console.log("true to number is ");
+console.log("false to number is ");
+console.log("null to number is ");
+console.log("undefined to number is ");
+console.log("'   20   ' to number is ");
+console.log("'      ' to number is ");
+console.log("'   30d   ' to number is ");
+
+/* == */
+console.log("null to boolean is ");
+console.log("undefined to boolean is ");
+console.log("0 to boolean is ");
+console.log("-0 to boolean is ");
+console.log("NaN to boolean is ");
+console.log("'' to boolean is ");
+console.log("' ' to boolean is ");
+console.log("17 to boolean is ");
+console.log("'Hello' to boolean is ");
