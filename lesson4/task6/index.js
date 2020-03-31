95cd6f14ec721013510770772348b58cd288f9b3
job-artem <job.artem.2019@gmail.com>
2020-03-31 15:08:30 +0300

    1

--- lesson4/task6/index.js
+++ lesson4/task6/index.js
@@ -4,13 +4,26 @@ let result = 0;
 for (let i = m; i <= n; i++) {
 
     if (i % 2 === 0 && i % 4 !== 0) {
+        if (i % 5 === 0) {
+            console.log(i);
+            continue;
+        }
         result = result + i;
     } else if (i % 3 === 0) {
+        if (i % 5 === 0) {
+            console.log(i);
+            continue;
+        }
         result = result - i;
     } else if (i % 4 === 0) {
+        if (i % 5 === 0) {
+            console.log(i);
+            continue;
+        }
         result = result * i;
     } else if (i % 5 === 0) {
         console.log(i);
+        continue;
     } else {
         continue;
     }
