62dda884dc79116993ce7a6e32b61ba606c23192
job-artem <job.artem.2019@gmail.com>
2020-03-31 19:25:23 +0300

    1

--- lesson5/task5/index.js
+++ lesson5/task5/index.js
@@ -1,7 +1,5 @@
 const getSum = (a,b) => {
     sum = 0;
-    a = prompt('Enter value');
-    b = prompt('Enter value');
     for(let i = a; i<=b; i++){
         if(i%2===0){
             sum = sum + i;
