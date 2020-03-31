250911c4c5f08f7ccf3e6095a58c681f1b981de8
job-artem <job.artem.2019@gmail.com>
2020-03-31 16:50:52 +0300

    1

--- lesson4/task5/index.js
+++ lesson4/task5/index.js
@@ -1,18 +1,16 @@
-const m = 3;
-const n = 10;
+const m = 14;
+const n = 36;
 let result = 0;
 for (let i = m; i <= n; i++) {
     if ((i % 2 == 0) && (i % 4 !== 0)) {
         if (i % 5 == 0) {
             console.log(i)
-            continue;
         } else {
             result = result + i;
         }
     } else if (i % 3 == 0) {
         if (i % 5 == 0) {
             console.log(i)
-            continue;
         } else {
             result = result - i;
         }
@@ -20,14 +18,12 @@ for (let i = m; i <= n; i++) {
     } else if (i % 4 == 0) {
         if (i % 5 == 0) {
             console.log(i)
-            continue;
         } else {
             result = result * i;
         }
 
     } else if (i % 5 == 0) {
         console.log(i)
-        continue;
     } else {
         continue;
     }
