479228151d4641581d774cd8bf26787145fe355b
job-artem <job.artem.2019@gmail.com>
2020-03-31 20:10:48 +0300

    task9 lesson5

---
+++ lesson5/task9/index.js
@@ -0,0 +1,12 @@
+function findDivCount(a,b,n) {
+    let count = 0;
+    for (let i = a; i <= b; i++){
+        if(i%n === 0){
+            count++;
+        }
+        else{
+            continue;
+        }
+    }
+    return count;
+}
