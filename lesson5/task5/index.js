a3d3e84a1c4e4bcbc101431e6f45d6e1035ff973
job-artem <job.artem.2019@gmail.com>
2020-03-31 19:07:09 +0300

    task5 less5

---
+++ lesson5/task5/index.js
@@ -0,0 +1,11 @@
+function getSum (a,b){
+    for(let i = a; i<=b; i++){
+        if(i%2===0){
+            sum +=i;
+        }        
+        else{
+            continue;
+        }
+    }
+    return sum;
+}
