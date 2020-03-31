9485fb8bfd089c01818ff7f398fb3b6cee0f8cb8
job-artem <job.artem.2019@gmail.com>
2020-03-31 15:15:37 +0300

    changed algorithm

--- lesson4/task5/index.js
+++ lesson4/task5/index.js
@@ -4,13 +4,32 @@ let result = 0;
 for (let i = m; i <= n; i++) {
 
     if (i % 2 === 0 && i % 4 !== 0) {
-        result = result + i;
+        if (i % 5 === 0) {
+            console.log(i);
+            continue;
+        } else {
+            result = result + i;
+        }
+
     } else if (i % 3 === 0) {
-        result = result - i;
+        if (i % 5 === 0) {
+            console.log(i);
+            continue;
+        } else {
+            result = result - i;
+        }
+
     } else if (i % 4 === 0) {
-        result = result * i;
+        if (i % 5 === 0) {
+            console.log(i);
+            continue;
+        } else {
+            result = result * i;
+        }
+
     } else if (i % 5 === 0) {
         console.log(i);
+        continue;
     } else {
         continue;
     }
