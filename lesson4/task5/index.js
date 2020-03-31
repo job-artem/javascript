c923cee407e065ef6c6aba17c9b94f11661eb454
job-artem <job.artem.2019@gmail.com>
2020-03-31 16:39:53 +0300

    task5 less4

--- lesson4/task5/index.js
+++ lesson4/task5/index.js
@@ -1,34 +1,35 @@
-const m = 2;
-const n = 30;
+const m = 3;
+const n = 10;
 let result = 0;
 for (let i = m; i <= n; i++) {
-
-    if (i % 2 === 0 && i % 4 !== 0) {
-        if (i % 5 === 0) {
-            console.log(i);
+    if ((i % 2 == 0) && (i % 4 !== 0)) {
+        if (i % 5 == 0) {
+            console.log(i)
             continue;
         } else {
             result = result + i;
+            continue;
         }
-
-    } else if (i % 3 === 0) {
-        if (i % 5 === 0) {
-            console.log(i);
+    } else if (i % 3 == 0) {
+        if (i % 5 == 0) {
+            console.log(i)
             continue;
         } else {
             result = result - i;
+            continue;
         }
 
-    } else if (i % 4 === 0) {
-        if (i % 5 === 0) {
-            console.log(i);
+    } else if (i % 4 == 0) {
+        if (i % 5 == 0) {
+            console.log(i)
             continue;
         } else {
             result = result * i;
+            continue;
         }
 
-    } else if (i % 5 === 0) {
-        console.log(i);
+    } else if (i % 5 == 0) {
+        console.log(i)
         continue;
     } else {
         continue;
--- lesson4/task8/index.js
+++ lesson4/task8/index.js
@@ -1,5 +1,5 @@
 let sum = 0;
-for (let i = 0; i <= 1000; i++) {
+for (let i = 1; i <= 1000; i++) {
     sum = sum + i;
 }
 let ost = sum % 1234;
