17c6b11715ceac022f8f0de1ae57dde48e559106
job-artem <job.artem.2019@gmail.com>
2020-04-01 17:42:19 +0300

    task17 lesson6

---
+++ lesson6/task17/index.js
@@ -0,0 +1,27 @@
+function removeDuplicates(array) {
+    if (!Array.isArray(array)) {
+        return null;
+    }
+    let new_arr = [];
+    for (let copy = 0; copy < array.length; copy++) {
+        new_arr[copy] = array[copy];
+    }
+    for (let i = 0; i < new_arr.length - 1; i++) {
+        for (let j = i + 1; j < new_arr.length; j++) {
+            if (new_arr[i] === new_arr[j]) {
+                new_arr.splice(j, 1);
+                j--;
+            }
+        }
+    }
+    return new_arr;
+}
+
+
+function uniqueCount(array) {
+    if (!Array.isArray(array)) {
+        return null;
+    }
+    let new_array = removeDuplicates(array);
+    return new_array.length;
+}
