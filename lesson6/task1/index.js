b530e331fd203689cbcda71d744ef6243c1bf50e
job-artem <job.artem.2019@gmail.com>
2020-04-01 10:46:51 +0300

    task1 less6

---
+++ lesson6/task1/index.js
@@ -0,0 +1,8 @@
+function getArrayBounds(arr) {
+    if (Array.isArray(arr)) {
+        boundsArr = [arr.length, arr[0], arr[arr.length - 1]]
+        return boundsArr;
+    } else {
+        return null;
+    }
+}
