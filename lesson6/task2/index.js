7506ba72a411b9d0ba363c8fde79c0e0a769288c
job-artem <job.artem.2019@gmail.com>
2020-04-01 11:00:55 +0300

    task2 lesson6

---
+++ lesson6/task2/index.js
@@ -0,0 +1,10 @@
+function getSum(arr) {
+    if (!Array.isArray(arr)) {
+        return null;
+    }
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+        sum = sum + arr[i];
+    }
+    return sum;
+}
