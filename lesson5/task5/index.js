345ba51af8f8d0e22e9c001c6e395ebd5d30f33e
job-artem <job.artem.2019@gmail.com>
2020-04-01 10:27:16 +0300

    1

--- lesson5/task5/index.js
+++ lesson5/task5/index.js
@@ -1,6 +1,6 @@
 let getSum = (begin, end) => {
     sum = 0;
-    for (let summator = begin; i <= end; i++) {
+    for (let summator = begin; summator <= end; summator++) {
         if (summator % 2 === 0) {
             sum = sum + summator;
         } else {
