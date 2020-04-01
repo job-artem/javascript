69b6bb5cd287a5e0cad7aa1b97f6ff58263f7b2d
job-artem <job.artem.2019@gmail.com>
2020-04-01 16:08:20 +0300

    1

--- lesson6/task16/index.js
+++ lesson6/task16/index.js
@@ -2,7 +2,7 @@ function removeDuplicates(array) {
     if (!Array.isArray(array)) {
         return null;
     }
-    if ((Array.isArray(array)) && (array.length > 0)) {
+    if (Array.isArray(array)) {
         for (i = 0; i < array.length - 1; i++) {
             for (j = i + 1; j < array.length; j++) {
                 if (array[i] === array[j]) {
