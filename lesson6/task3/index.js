9210fc9d9abf307a9111939e43175c2eabba0bad
job-artem <job.artem.2019@gmail.com>
2020-04-01 11:20:51 +0300

    task3 less6

---
+++ lesson6/task3/index.js
@@ -0,0 +1,9 @@
+function getSpecialNumbers(from, to) {
+    let specialNumbers = [];
+    for (let i = from; i <= to; i++) {
+        if (i % 3 === 0) {
+            specialNumbers.push(i);
+        }
+    }
+    return specialNumbers;
+}
