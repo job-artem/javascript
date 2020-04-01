b8766b510e61b0f62b6a46a623652587337e2725
job-artem <job.artem.2019@gmail.com>
2020-04-01 12:33:05 +0300

    1

--- lesson6/task8/index.js
+++ lesson6/task8/index.js
@@ -2,12 +2,14 @@ function increaseEvenEl(arr, delta) {
     if (!Array.isArray(arr)) {
         return null;
     }
+    new_arr = [];
     for (let el of arr) {
         if (el % 2 === 0) {
             el = el + delta;
+            new_arr.push(el);
         } else {
-            continue;
+            new_arr.push(el);
         }
     }
-    return arr;
+    return new_arr;
 }
