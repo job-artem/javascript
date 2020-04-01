ccf6a44287c90c48bf6b5d352abf3a474e12f6ba
job-artem <job.artem.2019@gmail.com>
2020-04-01 15:53:54 +0300

    task16 less6

---
+++ lesson6/task16/index.js
@@ -0,0 +1,17 @@
+function removeDuplicates(array) {
+    if (!Array.isArray(array)) {
+        return null;
+    }
+    if ((Array.isArray(array)) && (array.length > 0)) {
+        for (i = 0; i < array.length - 1; i++) {
+            for (j = i + 1; j < array.length; j++) {
+                if (array[i] === array[j]) {
+                    array.splice(j, 1);
+                    i--;
+                    j--;
+                }
+            }
+        }
+        return array;
+    }
+}
