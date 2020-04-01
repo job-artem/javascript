56ad0d564e5d44a14a6107df5a1f1d8ca464f340
job-artem <job.artem.2019@gmail.com>
2020-04-01 16:13:40 +0300

    1

--- lesson6/task16/index.js
+++ lesson6/task16/index.js
@@ -4,10 +4,11 @@ function removeDuplicates(array) {
     }
     if (Array.isArray(array)) {
         for (i = 0; i < array.length - 1; i++) {
+            let count = 0;
             for (j = i + 1; j < array.length; j++) {
                 if (array[i] === array[j]) {
                     array.splice(j, 1);
-                    i--;
+                    count++;
                     j--;
                 }
             }
