4f5560e5a3fe811fdc396ccc2e8e02da8aeaad69
job-artem <job.artem.2019@gmail.com>
2020-04-01 12:50:29 +0300

    task9 less6

---
+++ lesson6/task9/index.js
@@ -0,0 +1,12 @@
+function reverseArray(arr) {
+    if (!Array.isArray(arr)) {
+        return null;
+    }
+    if (Array.isArray(arr)) {
+        new_arr = [];
+        for (let i = 0; i < arr.length; i++) {
+            new_arr.unshift(arr[i]);
+        }
+        return new_arr;
+    }
+}
