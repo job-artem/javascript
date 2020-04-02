093a2a33e9c8c6df68b593596b07d8ad4b355983
job-artem <job.artem.2019@gmail.com>
2020-04-02 18:48:56 +0300

    1

--- lesson7/task2/index.js
+++ lesson7/task2/index.js
@@ -1,9 +1,2 @@
-const sortDesc = numbers => {
-
-    let new_numbers = [];
-    for (let i = 0; i < numbers.length; i++) {
-        new_numbers[i] = numbers[i];
-    }
-    new_numbers.sort((a, b) => b - a);
-    return new_numbers;
-}
+const sortDesc = numbers => 
+    numbers.slice().sort((a, b) => b - a);
