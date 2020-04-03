a4cf4cbfe87931a458840b234b2eb60ec69cacb6
job-artem <job.artem.2019@gmail.com>
2020-04-03 16:51:33 +0300

    1

--- lesson7/task9/index.js
+++ lesson7/task9/index.js
@@ -1,13 +1,8 @@
 const withdraw = (clients, balances, client, amount) => {
-    if (Array.isArray(clients) && Array.isArray(balances) && (typeof(client) === 'string') && (typeof(amount) === 'number')) {
-        let curr = clients.indexOf(client)
-        if (balances[curr] < amount) {
-            return -1;
-        } else {
-             
-            balances[curr] = balances[curr] - amount;
-            return balances[curr];
-        }
+    let curr = clients.indexOf(client)
+    if (balances[curr] < amount) {
+        return -1;
     }
-
+    balances[curr] = balances[curr] - amount;
+    return balances[curr];
 }
