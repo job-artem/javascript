7cf762cff8b6e0c32f97c4fd2de30fcfc457e765
job-artem <job.artem.2019@gmail.com>
2020-04-06 21:19:39 +0300

    1

--- lesson8/task11/index.js
+++ lesson8/task11/index.js
@@ -2,16 +2,17 @@ const pickProps = (obj, arr) => {
     
     obj_keys = Object.keys(obj);
     obj_ret = {};
-    
+    let count = 0;
     arr.forEach(key =>{
         obj_keys.forEach(el=> {
             if(el === key){
                 obj_ret[key] = obj[key];
-            }
-            else{
-
+                count++
             }
         })
     })
+    if(count === 0){
+        return {};
+    }
     return obj_ret;
 }
