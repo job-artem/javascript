b2ce77b20f4d29efac5e2895acd5b9a9561bd5fd
job-artem <job.artem.2019@gmail.com>
2020-04-06 17:00:28 +0300

    task9 lesson8

--- lesson8/task9/index.js
+++ lesson8/task9/index.js
@@ -1,19 +1,31 @@
-const mergeObjectsV1 = (obj1, obj2) => {
-    let obj_res = Object.assign({}, obj1, obj2);
-    return obj_res;
-}
+const compareObjects = (obj1, obj2) => {
+    const keys1_arr = Object.keys(obj1); //Get key arrays for both objects
+    const keys2_arr = Object.keys(obj2);
 
-const mergeObjectsV2 = (obj1, obj2) => {
-    let obj_res = Object.assign({}, obj2, obj1);
-    return obj_res;
-}
 
-const mergeObjectsV3 = (obj1, obj2) => {
-    let obj_res = {...obj1, ...obj2 };
-    return obj_res;
-}
+    const values1_arr = [];
+    const values2_arr = [];
 
-const mergeObjectsV4 = (obj1, obj2) => {
-    let obj_res = {...obj2, ...obj1 };
-    return obj_res;
+    keys1_arr.forEach(el => values1_arr.push(obj1[el]));
+    keys2_arr.forEach(el => values2_arr.push(obj2[el]));
+
+
+    if (keys1_arr.length === keys2_arr.length) { // Comparing keys for both objects
+        let counter = 0;
+        for (let i = 0; i < keys1_arr.length; i++) {
+            if (keys1_arr[i] !== keys2_arr[i]) {
+                counter++
+            }
+            if (values1_arr[i] !== values2_arr[i]) {
+                counter++
+            }
+        }
+        if (counter === 0) {
+            return true;
+        } else {
+            return false;
+        }
+    } else {
+        return false;
+    }
 }
