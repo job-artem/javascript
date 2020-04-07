8e10f3c4b1f520ae5991c73db48a6c66c4b4a706
job-artem <job.artem.2019@gmail.com>
2020-04-07 14:25:09 +0300

    1

--- lesson8/task12 copy/index.js
+++
@@ -1,9 +0,0 @@
-const getAdults = (obj) => {
-    const returnObj = {};
-    for (key in obj) {
-        if (obj[key] >= 18) {
-            returnObj[key] = obj[key];
-        }
-    }
-    return returnObj;
-}
