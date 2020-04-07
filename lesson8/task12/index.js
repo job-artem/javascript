23cba12548e639d0f6d633c70ca4a3d8d97a2f10
job-artem <job.artem.2019@gmail.com>
2020-04-07 10:41:04 +0300

    1

--- lesson8/task12/index.js
+++ lesson8/task12/index.js
@@ -1,23 +1,12 @@
-function isEmpty(obj) {
-    for (let key in obj) {
-      // если тело цикла начнет выполняться - значит в объекте есть свойства
-      return false;
-    }
-    return true;
-  }
-
 const getAdults = (obj) => {
-    if(isEmpty(obj)){
-        return {};
-    }
     ret_obj = {};
-    
     obj_keys = Object.keys(obj);
-    obj_keys.forEach(key=> {
-        if(obj[key] >= 18){
+
+    obj_keys.forEach(key => {
+        if (obj[key] >= 18) {
             ret_obj[key] = obj[key];
         }
     })
     return ret_obj;
-    
+
 }
