d7bfa8fe4bf81f61ddbedba06cae18f9435b7167
job-artem <job.artem.2019@gmail.com>
2020-04-07 10:34:30 +0300

    1

--- lesson8/task11/index.js
+++ lesson8/task11/index.js
@@ -4,9 +4,7 @@ const pickProps = (obj, arr) => {
     const obj_ret = {};
     arr.forEach(el => {
         if (obj_keys.includes(el)) {
-            Object.assign(obj_ret, { el: obj[el] })
-        } else {
-            continue;
+            obj_ret[el] = obj[el];
         }
     })
     return obj_ret;
