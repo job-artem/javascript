d0a54f3dc62d828edf3cc1a7a79060e6ab4ace61
job-artem <job.artem.2019@gmail.com>
2020-04-07 11:00:06 +0300

    1

--- lesson8/task12/index.js
+++ lesson8/task12/index.js
@@ -2,11 +2,16 @@ const getAdults = (obj) => {
     ret_obj = {};
     obj_keys = Object.keys(obj);
 
-    obj_keys.forEach(key => {
-        if (obj[key] >= 18) {
-            ret_obj[key] = obj[key];
+    obj_values = [];
+    for (let i = 0; i < obj_keys.length; i++) {
+        obj_values.push(obj[obj_keys[i]]);
+    }
+
+    for (let j = 0; j < obj_keys.length; j++) {
+        if (obj_values[j] >= 18) {
+            ret_obj[obj_keys[j]] = obj_values[j];
         }
-    })
+    }
     return ret_obj;
 
 }
