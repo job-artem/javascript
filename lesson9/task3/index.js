e32780abe3da1568cfcd2878b534d57b4f6da453
job-artem <job.artem.2019@gmail.com>
2020-04-08 16:02:15 +0300

    task3 less9

--- lesson9/task3/index.js
+++ lesson9/task3/index.js
@@ -1,25 +1,35 @@
-const customers = {
-    'customer-id-1': {
-        name: 'William',
-        age: 54
-    },
-    'customer-id-2': {
-        name: 'Tom',
-        age: 17
-    },
-};
+const findMinAgeIndex = (arr) => {
+    let min = arr[0].age;
+    let index = 0;
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i].age < min) {
+            min = arr[i].age;
+            index = i;
+        }
+    }
+    return index;
+}
 
 const getCustomersList = (obj) => {
     const arrCustomers = Object.entries(obj);
     console.log(arrCustomers);
+    const objElem = {};
+    const arrFormatted = [];
+    arrCustomers.forEach(el => {
+        name = el[1].name;
+        age = el[1].age;
+        id = el[0];
+        arrFormatted.push({ name, age, id });
+    });
+    // console.log(arrFinal);
+    arrCopy = [...arrFormatted];
+    const sortedArray = [];
+    while (arrCopy.length > 0) {
+        const minAgeIndex = findMinAgeIndex(arrCopy);
+        sortedArray.push(arrCopy[minAgeIndex]);
+        arrCopy.splice(minAgeIndex, 1);
+    }
+    console.log(sortedArray);
 
-    const [key, ...rest] = arrCustomers[0];
-    console.log(key);
-    console.log(rest);
-    const [objInRest] = rest;
-    console.log(objInRest);
-    const { name, age } = objInRest;
-    console.log(name, age);
 }
-
 getCustomersList(customers);
