b7db27fa62b032afb5baad7deebce6fcaa77a74e
job-artem <job.artem.2019@gmail.com>
2020-04-09 18:56:59 +0300

    1

--- lesson9/task3-map/index.js
+++ lesson9/task3-map/index.js
@@ -1,15 +1,12 @@
 const getCustomersList = (obj) => {
+    const resArr = [];
     const arrEntries = Object.entries(obj);
-    arrEntries
-        .map(el => {
+    arrEntries.map(el => {
             el.push(el[1].name);
             el.push(el[1].age);
             el.splice(1, 1);
         }) // Получили двумерный массив
-        .sort((a, b) => a[2] - b[2]) //Отсортировали по age
-    const resArr = [];
-
-
+    arrEntries.sort((a, b) => a[2] - b[2]) //Отсортировали по age
     arrEntries.forEach(el => {
         id = el[0];
         name = el[1];
